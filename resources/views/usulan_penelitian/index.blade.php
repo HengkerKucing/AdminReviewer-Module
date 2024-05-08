21d6a553fa1616b40565ea4671aaa8b43a5248dd
Riza Sukmawardani <umawar1229@gmail.com>
2024-05-08 12:11:18 +0700

    usulan-penelitian

--- resources/views/usulan_penelitian/index.blade.php
+++ resources/views/usulan_penelitian/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
+@push('css')
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+@endpush
+
 @section('content')
-
-    <div class="container d-flex flex-column gap-5">
-        <div class="row pt-5 mb-3">
-            <div class="m-0 p-0 row col d-flex align-items-center justify-content-between">
-                <h3>Daftar Usulan</h3>
-                <p class="mb-0">Home / Daftar Usulan</p>
-
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
@@ -16,138 +16,155 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        // Kasih filter
+                       
                     </ol>
-                </div>
->>>>>>> e8cff69340b21e015a73f5a4ad45033d37b73187
-            </div>
-        </div>
-
-        <div class="row mb-1">
-            <div class="col-12 col-md-4 card card-outline">
-                <div class="card-header">
-                    <h5 class="m-0 text-bold">Tahun Pelaksanaan</h5>
-                </div>
-                <div class="card-body">
-                    <select class="form-control" id="tahunSelect">
-                        <option>2024</option>
-                        <option>2023</option>
-                        <option>2022</option>
-                        <option>2021</option>
-                        <option>2020</option>
-                    </select>
-                </div>
-            </div>
-            <div class="col-md-1"></div>
-            <div class="col-12 col-md-7 card card-outline">
-                <div class="card-header">
-                    <h5 class="m-0 text-bold">Usulan Skema Baru</h5>
-                </div>
-                <div class="card-body d-flex flex-row flex-wrap" style="gap: 10px;">
-                    @foreach (json_decode(MenuHelper::getUsulanPenelitian()) as $item)
-                        <button type="button"
-                            class="col-sm-12 col-md btn btn-{{ $item->skema_color }} d-flex flex-column align-items-center"
-                            data-toggle="modal" data-target="#Modal{{ $item->skema_id }}">
-                            <i class="fas fa-{{ $item->skema_icon }}"></i>
-                            <span>{{ $item->skema_nama }}</span>
-                        </button>
-
-                        <div class="modal fade" id="Modal{{ $item->skema_id }}" tabindex="-1"
-                            aria-labelledby="{{ $item->skema_id }}ModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">{{ $item->skema_nama }}</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body d-flex flex-column" style="gap: 10px">
-                                        @foreach ($item->trx_skema as $skema)
-                                            <a class="btn btn-info"
-                                                href="{{ url('/tambah_usulan') }}?skema_id={{ $item->skema_id }}&step=1&usulan_id=">{{ $skema[0] }}</a>
-                                        @endforeach
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-
-        <div class="row mb-1">
-            <div class="col-12 card card-outline">
-                <div class="card-body" style="overflow: auto">
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Judul</th>
-                                <th scope="col">Abstrak</th>
-                                <th scope="col">Skema</th>
-                                <th scope="col">Anggota</th>
-                                <th scope="col">Pendanaan</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if ($count_usulan > 0)
-                                @foreach ($usulan as $key => $item)
-                                    <tr class="m-auto">
-                                        <td>{{ $key + 1 }}</td>
-                                        <td>{{ $item['usulan_judul'] }}</td>
-                                        <td>{{ $item['usulan_abstrak'] }}</td>
-                                        <td>{{ $item['nama_skema'] }}</td>
-                                        <td>
-                                            <ul>
-                                                <li><b> *Ketua</b> {{ $item['ketua']->dosen_nama }}
-                                                    ({{ $item['ketua']->dosen_id }})
-                                                </li>
-                                                @foreach ($item['anggota_dosen'] as $val)
-                                                    <li>
-                                                        {{ $val }}
-                                                    </li>
-                                                @endforeach
-                                                @foreach ($item['anggota_mhs'] as $val)
-                                                    <li>
-                                                        {{ $val }}
-                                                    </li>
-                                                @endforeach
-                                            </ul>
-                                        </td>
-                                        <td>
-                                            @if ($item['count_pendanaan'] > 0)
-                                                {{ $item['pendanaan']->first()->pendanaan_value }}
-                                            @else
-                                                -
-                                            @endif
-
-                                        </td>
-                                        <td>
-                                            {{ $item['status_id'] }}
-                                        </td>
-                                        <td>
-                                            @if ($item['status_id'] < 2)
-                                                <a href="{{ url('/tambah_usulan') }}?skema_id={{ $item['skema_id'] }}&step=1&usulan_id={{ $item['usulan_id'] }}"
-                                                    class="btn btn-warning">Edit Usulan</a>
-                                            @else
-                                                <button class="btn btn-warning" disabled>Edit Usulan</button>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            @else
-                                <tr>
-                                    <td colspan="8" class="text-center">Data Kosong</td>
-                                </tr>
-                            @endif
-
-                        </tbody>
-                    </table>
                 </div>
             </div>
         </div>
     </div>
+    
+    <section class="content">
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="card card-danger card-outline filter-container">
+                        <div class="card-header">
+                            <h1 class="card-title font-weight-bold">Filter</h1>
+                        </div>
+                        <div class="card-body">
+                            <!-- Form filter -->
+                            <form id="filter-form">
+                                <div class="form-group">
+                                    <label for="filter-title">Tahun:</label>
+                                    <select class="form-control" id="filter-title">
+                                        <option value="">-- Pilih tahun --</option>
+                                        <!-- isi dengan data judul -->
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="filter-title">Skema:</label>
+                                    <select class="form-control" id="filter-title">
+                                        <option value="">-- Pilih Skema --</option>
+                                        <!-- isi dengan data judul -->
+                                    </select>
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label for="filter-year">Status:</label>
+                                    <select class="form-control" id="filter-year">
+                                        <option value="">-- Pilih Status --</option>
+                                        <!-- isi dengan data tahun -->
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Judul:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="datatable-main"></label>
+                                </div>
+                                <div>
+                                    <label>Nama Anggota:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="datatable-main"></label>
+                                </div>
+                                <button type="submit" class="btn btn-danger">Filter</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="card card-primary card-outline">
+                        <div class="card-header">
+                            <!-- Kodingan di samping fitur filter -->
+                            <div class="row">
+                                <div class="col-lg-3 col-6">
+                                    <div class="small-box bg-info">
+                                        <div class="inner">
+                                            <h3>150</h3>
+                                            <p>PTK</p>
+                                        </div>
+                                        <div class="icon">
+                                            <i class="ion ion-bag"></i>
+                                        </div>
+                                        <!-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-6">
+                                    <div class="small-box bg-success">
+                                        <div class="inner">
+                                            <h3>53<sup style="font-size: 20px">%</sup></h3>
+                                            <p>Bounce Rate</p>
+                                        </div>
+                                        <div class="icon">
+                                            <i class="ion ion-stats-bars"></i>
+                                        </div>
+                                        <!-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-6">
+                                    <div class="small-box bg-warning">
+                                        <div class="inner">
+                                            <h3>44</h3>
+                                            <p>User Registrations</p>
+                                        </div>
+                                        <div class="icon">
+                                            <i class="ion ion-person-add"></i>
+                                        </div>
+                                        <!-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-6">
+                                    <div class="small-box bg-danger">
+                                        <div class="inner">
+                                            <h3>65</h3>
+                                            <p>Unique Visitors</p>
+                                        </div>
+                                        <div class="icon">
+                                            <i class="ion ion-pie-graph"></i>
+                                        </div>
+                                        <!-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Kodingan di samping fitur filter -->
+                            <table id="datatable-main" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Judul</th>
+                                        <th>Skema</th>
+                                        <th>Anggota</th>
+                                        <th>Pendanaan</th>
+                                        <th>Status</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($usulan as $item)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $item->usulan_judul }}</td>
+                                            <td>{{ $item->skema->trx_skema_nama }}</td>
+                                            <td>
+                                                <!-- Isi dengan data anggota -->
+                                            </td>
+                                            <td>{{ $item->usulan_pendanaan }}</td>
+                                            <td>
+                                                <!-- Isi dengan status -->
+                                            </td>
+                                            <td>
+                                                <div class="flex items-col">
+                                                    <button type="button" class="btn btn-block btn-sm btn-outline-info mr-2" onclick="window.location.href = 'ref-skema-file'"><i class="fas fa-eye"></i></button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
+
+@push('js')
+    <!-- Add your JavaScript here -->
+@endpush
