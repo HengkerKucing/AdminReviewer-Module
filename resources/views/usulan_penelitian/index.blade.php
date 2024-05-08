aac78cd7e590e02f4ed83300afff417ec4781129
IsnaJesika <jesikaparasinta@gmail.com>
2024-05-08 10:46:07 +0700

    Add Review Usulan

--- resources/views/usulan_penelitian/index.blade.php
+++ resources/views/usulan_penelitian/index.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
+
 @push('css')
-<!-- DataTables -->
-    <link rel="stylesheet" href="{{ asset('') }}plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="{{ asset('') }}plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-    <link rel="stylesheet" href="{{ asset('') }}plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 @endpush
+
 @section('content')
     <div class="content-header">
         <div class="container-fluid">
@@ -20,24 +22,86 @@
             </div>
         </div>
     </div>
-    <div class="content">
-        <div class="container-fluid">
+    <section class="content">
+<div class="container-fluid">
+
+<div class="row">
+<div class="col-lg-3 col-6">
+
+<div class="small-box bg-info">
+<div class="inner">
+<h3>150</h3>
+<p>PTK</p>
+</div>
+<div class="icon">
+<i class="ion ion-bag"></i>
+</div>
+<a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+</div>
+</div>
+
+<div class="col-lg-3 col-6">
+
+<div class="small-box bg-success">
+<div class="inner">
+<h3>53<sup style="font-size: 20px">%</sup></h3>
+<p>Bounce Rate</p>
+</div>
+<div class="icon">
+<i class="ion ion-stats-bars"></i>
+</div>
+<a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+</div>
+</div>
+
+<div class="col-lg-3 col-6">
+
+<div class="small-box bg-warning">
+<div class="inner">
+<h3>44</h3>
+<p>User Registrations</p>
+</div>
+<div class="icon">
+<i class="ion ion-person-add"></i>
+</div>
+<a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+</div>
+</div>
+
+<div class="col-lg-3 col-6">
+
+<div class="small-box bg-danger">
+<div class="inner">
+<h3>65</h3>
+<p>Unique Visitors</p>
+</div>
+<div class="icon">
+<i class="ion ion-pie-graph"></i>
+</div>
+<a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+</div>
+</div>
+
+</div>
+
             <div class="row">
                 <div class="col-md-12">
                     <div class="card card-primary card-outline">
                         <div class="card-header">
-                        <table id="datatable-main" class="table table-bordered table-striped ">
+                            <table id="datatable-main" class="table table-bordered table-striped">
                                 <thead>
-                                    <th>No</th>
-                                    <th>Judul</th>
-                                    <th>Skema</th>
-                                    <th>Anggota</th>
-                                    <th>Pendanaan</th>
-                                    <th>Status</th>
-                                    <th>Aksi</th>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Judul</th>
+                                        <th>Skema</th>
+                                        <th>Anggota</th>
+                                        <th>Pendanaan</th>
+                                        <th>Status</th>
+                                        <th>Aksi</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                @foreach ($usulan as $item)
+                                    @foreach ($usulan as $item)
                                         <tr>
                                             <td>{{ $loop->iteration }}</td>
                                             <td>{{ $item->usulan_judul }}</td>
@@ -57,28 +121,13 @@
                                                     {{ $status->status_id }}
                                                 @endforeach
                                             </td>
-
                                             <td>
                                                 <div class="flex items-col">
-                                                <button type="button" class="btn btn-block btn-sm btn-outline-info mr-2";
-                                                    onclick="window.location.href = 'ref-skema-file'"><i class="fas fa-eye"></i>
-                                                </button>
+                                                    <button type="button" class="btn btn-block btn-sm btn-outline-info mr-2" onclick="window.location.href = 'ref-skema-file'"><i class="fas fa-eye"></i></button>
                                                 </div>
                                             </td>
-                                            <style>
-                                            .flex {
-                                                display: flex;
-                                            }
-
-                                            .flex-col {
-                                                flex-direction: column;
-                                            }
-                                            .mb-2 {
-                                                margin-right: 5px;
-                                            }
-                                            </style>
                                         </tr>
-                                @endforeach
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
@@ -86,8 +135,9 @@
                 </div>
             </div>
         </div>
-    </div>
-    </div>
+    </section>
 @endsection
+
 @push('js')
+    <!-- Add your JavaScript here -->
 @endpush
