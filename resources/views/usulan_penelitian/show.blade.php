1f128295f983ccec1b08639f7d6f8b602aa175ae
IsnaJesika <jesikaparasinta@gmail.com>
2024-05-27 12:09:21 +0700

    usulan penelitian pendanaan

--- resources/views/usulan_penelitian/show.blade.php
+++ resources/views/usulan_penelitian/show.blade.php
@@ -23,6 +23,7 @@
     <div class="content">
         <div class="container-fluid">
             <div class="row">
+                <!-- Data Penelitian Section -->
                 <div class="col-md-6">
                     <div class="card card-primary card-outline">
                         <div class="card-header">
@@ -30,20 +31,21 @@
                         </div>
                         <div class="card-body">
                             <div class="row mb-3">
-                                <div class="col-md-4 font-weight-bold">Nama Skema:</div>
+                                <div class="col-md-4 font-weight-bold">Skema:</div>
                                 <div class="col-md-8">{{ $skemaNama }}</div>
                             </div>
                             <div class="row mb-3">
-                                <div class="col-md-4 font-weight-bold">Judul Usulan:</div>
+                                <div class="col-md-4 font-weight-bold">Judul:</div>
                                 <div class="col-md-8">{{ $usulanJudul }}</div>
                             </div>
                             <div class="row mb-3">
-                                <div class="col-md-4 font-weight-bold">Abstrak Usulan:</div>
+                                <div class="col-md-4 font-weight-bold">Abstrak:</div>
                                 <div class="col-md-8">{{ $usulanAbstrak }}</div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <!-- Anggota Section -->
                 <div class="col-md-6">
                     <div class="card card-primary card-outline">
                         <div class="card-header">
@@ -51,8 +53,8 @@
                         </div>
                         <div class="card-body">
                             <div class="row mb-3">
-                                <div class="col-md-3 font-weight-bold">Dosen:</div>
-                                <div class="col-md-9">
+                                <div class="col-md-12">
+                                    <h6>Dosen</h6>
                                     <table class="table table-bordered">
                                         <thead>
                                             <tr>
@@ -67,6 +69,21 @@
                                                 <td>{{ $anggota->dosen->prodi->prodi_nama }}</td>
                                             </tr>
                                         @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-md-12">
+                                    <h6>Mahasiswa</h6>
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>Nama</th>
+                                                <th>Program Studi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
                                         @foreach ($usulanPenelitian->anggotaMahasiswa as $anggota)
                                             <tr>
                                                 <td>{{ $anggota->mahasiswa->mhs_nama }}</td>
@@ -77,18 +94,128 @@
                                     </table>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <!-- Capaian Section -->
+                <div class="col-md-6">
+                    <div class="card card-primary card-outline">
+                        <div class="card-header">
+                            <h5>Capaian</h5>
+                        </div>
+                        <div class="card-body">
                             <div class="row mb-3">
-                                <div class="col-md-3 font-weight-bold">Mahasiswa:</div>
-                                <div class="col-md-9">
+                                <div class="col-md-12">
+                                    <h6>Luaran Wajib</h6>
                                     <table class="table table-bordered">
                                         <thead>
                                             <tr>
-                                                <th>Nama</th>
-                                                <th>Program Studi</th>
+                                                <th>Jenis Luaran Wajib</th>
+                                                <th>Luaran</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <!-- Data for Mahasiswa -->
+                                            <tr>
+                                                <td>Jurnal Nasional Terakreditasi</td>
+                                                <td>{{ $luaranWajib }}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Produk Iptek-Sosbud</td>
+                                                <td>{{ $luaranTambahan }}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-md-12">
+                                    <h6>Luaran Tambahan</h6>
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>Jenis Luaran Tambahan</th>
+                                                <th>Luaran</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>HKI</td>
+                                                <td>{{ $luaranTambahan }}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-md-12">
+                                    <h6>IKU</h6>
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>IKU</th>
+                                                <th>Target Pengusul</th>
+                                                <th>Bukti</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Publikasi Ilmiah (IK-2.01.02) Nasional Terakreditasi</td>
+                                                <td>Jurnal nasional</td>
+                                                <td>{{ $iku }}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Pemakalah dalam pertemuan ilmiah (IK-2.01.02) Nasional</td>
+                                                <td>{{ $iku }}</td>
+                                                <td>{{ $iku }}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Teknologi Tepat Guna (IK-2.01.03)</td>
+                                                <td>tersedia</td>
+                                                <td>{{ $iku }}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Komponen Pendanaan Section -->
+                <div class="col-md-6">
+                    <div class="card card-primary card-outline">
+                        <div class="card-header">
+                            <h5>Komponen Pendanaan</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="row mb-3">
+                                <div class="col-md-12">
+                                    <h6>Komponen Pendanaan</h6>
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>Kategori</th>
+                                                <th>Jumlah</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Bahan habis pakai dan peralatan (Maks. 60%)</td>
+                                                <td>Rp 6.500.000</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Perjalanan (Maks. 30%)</td>
+                                                <td>Rp 1.000.000</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Lain-lain (Maks. 40%)</td>
+                                                <td>Rp 4.500.000</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Total Pendanaan</td>
+                                                <td>Rp 12.000.000</td>
+                                            </tr>
                                         </tbody>
                                     </table>
                                 </div>
@@ -97,66 +224,8 @@
                     </div>
                 </div>
             </div>
-            <!-- Repeat similar blocks for other sections -->
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card card-primary card-outline">
-                        <div class="card-header">
-                            <h5>Capaian</h5>
-                        </div>
-                        <div class="card-body">
-                            <div class="row mb-3">
-                                <div class="col-md-3 font-weight-bold">Luaran Wajib:</div>
-                                <div class="col-md-9">{{ $luaranWajib }}</div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-3 font-weight-bold">Luaran Tambahan:</div>
-                                <div class="col-md-9">{{ $luaranTambahan }}</div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-3 font-weight-bold">IKU:</div>
-                                <div class="col-md-9">{{ $iku }}</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card card-primary card-outline">
-                        <div class="card-header">
-                            <h5>Komponen Pendanaan</h5>
-                        </div>
-                        <div class="card-body">
-                            <div class="row mb-3">
-                                <div class="col-md-3 font-weight-bold">Bahan habis pakai dan peralatan:</div>
-                                <div class="col-md-9">{{ $luaranWajib }}</div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-3 font-weight-bold">Perjalanan:</div>
-                                <div class="col-md-9">{{ $luaranTambahan }}</div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-3 font-weight-bold">Lain-lain:</div>
-                                <div class="col-md-9">{{ $luaranTambahan }}</div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-3 font-weight-bold">Total Pendanaan:</div>
-                                <div class="col-md-9">{{ $luaranTambahan }}</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Berkas Usulan section -->
-            <div class="row">
-            <div class="col-md-6">
-                    <div class="card card-primary card-outline">
-                        <div class="card-header">
-                            <h5>Berkas Usulan</h5>
-                        </div>
-                        <!-- Berkas Usulan content -->
-                    </div>
-                </div>
-            </div>
+            
+               
         </div>
     </div>
 
@@ -191,8 +260,7 @@
 
             .mb-2 {
                 margin-right: 5px;
-                /*
-                                /* width: auto; */
+                /* width: auto; */
             }
         </style>
     @endpush
