68186ff87925bff3f04f5433d26990437a34182e
rizkyfaizullah <m.rizky.faizullah.s@gmail.com>
2024-05-29 18:38:00 +0700

    Fix typo

--- app/Models/ReviewUsulanNilai.php
+++ app/Models/ReviewUsulanNilai.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class RevieweUsulanNilai extends Model
+class ReviewUsulanNilai extends Model
 {
     use HasFactory;
 
